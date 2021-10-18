138b68c5c5f7b1ae9b0c5387e69d7fb3445fe52b
Andrei Korikov <kpanic666@gmail.com>
2021-10-18 20:22:57 +0500

    Initial Commit

---
+++ WordScrambleUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordScrambleUIKit
+//
+//  Created by Andrei Korikov on 18.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
